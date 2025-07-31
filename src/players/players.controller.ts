a66b3ae62f2772e39d07ecf059bfdfdb042887d8
Raul Cabral <raulcabralc@gmail.com>
2025-07-31 16:38:21 -0300

    feat: create player

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,23 +1,13 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { PlayersService } from "./players.service";
 import { CreatePlayerDTO } from "./dto/create-player.dto";
 
 @Controller("/jogadores")
 export class PlayersController {
-  constructor(readonly playersService: PlayersService) {}
-
-  @Get()
-  async playerGet() {}
+  constructor(private readonly playersService: PlayersService) {}
 
   @Post()
   createUpdatePlayer(@Body() body: CreatePlayerDTO) {
-    // this.playersService.create(body);
-    const { email, name, phoneNumber } = body;
-
-    return {
-      email,
-      name,
-      phoneNumber,
-    };
+    this.playersService.createUpdatePlayer(body);
   }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { CreatePlayerDTO } from "./dto/create-player.dto";
+import { Player } from "./interfaces/player.interface";
+import * as uuid from "uuid";
 
 @Injectable()
 export class PlayersService {
+  private players: Player[] = [];
+  private readonly logger = new Logger(PlayersService.name);
+
   constructor() {}
 
-  create(body: CreatePlayerDTO) {
-    return body;
+  createUpdatePlayer(body: CreatePlayerDTO): void {
+    this.logger.log("createsPlayerDTO:", body);
+
+    this.create(body);
+  }
+
+  private create(body: CreatePlayerDTO): Player {
+    const { name, email, phoneNumber } = body;
+
+    const player: Player = {
+      _id: uuid.v1(),
+      name,
+      email,
+      phoneNumber,
+      ranking: "A",
+      rankingPosition: 1,
+      photoUrl: "url da foto",
+    };
+
+    this.players.push(player);
+
+    return player;
   }
 }
